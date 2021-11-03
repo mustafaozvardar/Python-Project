0eabfeb3a38e185fb137077e0fb7833758b2e41b
mustafaozvardar <80473326+mustafaozvardar@users.noreply.github.com>
2021-11-03 15:16:34 +0200

    Add files via upload

---
+++ Python Projeler/Sisteme_Giris_İslemi.py
@@ -0,0 +1,41 @@
+print("***Giris Uygulaması***")
+print("1-Giriş Yap\t2-Çıkış Yap")
+#Kullanici ve Sifre değişkenlere verilerimizi atadık.
+kullanici="random"
+sifre="1234"
+hak=3
+sec=int(input("Secim: "))
+while True:
+    
+    if sec==1:
+        #Kullanıcıdan Sifre ve Kullanıcı Adı girmesini istedik.
+        userkullanici=input("Kullanıcı adı giriniz: ")
+        usersifre=input("Sifre giriniz: ")
+        if userkullanici==kullanici and usersifre!=sifre:
+            #Eğer şifre hatalı ise hakkını 1 azalttık.
+            print("Sifre hatali..")
+            hak-=1
+            print("Kalan Hak: ",hak)
+        elif userkullanici!=kullanici and usersifre==sifre:
+            #Eğer Kullanıcı Adı hatalı ise hakkını 1 azalttık.
+            print("Kullanıcı adı hatalı..")
+            hak-=1
+            print("Kalan Hak: ",hak)
+        elif userkullanici!=kullanici and usersifre!=sifre:
+            #Eğer Kullanıcı Adı ve Sifre hatalı ise hakkını 1 azalttık.
+            print("Kullanıcı adı ve Sifre hatali..")
+            hak-=1
+            print("Kalan Hak: ",hak)
+        else:
+            #Başta atamış olduğumuz değerle ile kullanıcan almış olduğumuz eşleştirildiğinde sisteme giriş yapıldı.
+            print("-->Sisteme HOŞGELDİNİZ..")
+            break
+        if hak==0:
+            #Hakkı bittiğinde sisteme girmesini engelledik.
+            print("Hakkınız Kalmadı..! Girişiniz Engellendi.")
+            break
+    elif sec==2:
+        print("Çıkıs yapıldı...")
+        break
+    else:
+        print("Hatali secim..!")
